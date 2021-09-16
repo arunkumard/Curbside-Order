1dae54b4479b85efbff0963b13cd8d5312a60972
Arun D <arunkumard@hotmail.com>
2021-09-16 18:03:28 +0000

    made changes to endpoints

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ class Test(Resource):
 
 
 def get_orders(order_type):
-  if order_type == "curbside":
+  if order_type == "arrival":
     file_address = 'Orders/curbside.json'
   else:
     file_address = 'errormsg.json'  
-  with open(file_address, 'r') as factfile:
-    data = json.load(factfile)
+  with open(file_address, 'r') as orderfile:
+    data = json.load(orderfile)
   order = random.choice(list(data['Orders']))
 
   return order 
@@ -39,7 +39,7 @@ class Orders(Resource):
   def get(self, order_type):
     return get_orders(order_type)
 
-api.add_resource(Orders, '/api/orders/<string:order_type>')
+api.add_resource(Orders, '/curbside/<string:order_type>')
 
 def run():
   app.run(host='0.0.0.0',port=7210)
